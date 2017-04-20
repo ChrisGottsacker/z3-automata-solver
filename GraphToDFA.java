03a6bebd1b63f41070952596c07f43bfecf23cea
salildureja <sdureja@wisc.edu>
2017-04-20 11:17:30 -0500

    Added loop for numStates

--- GraphToDFA.java
+++ GraphToDFA.java
@@ -7,130 +7,136 @@ class GraphToDFA
     System.out.println("--------------------------------------------------");
     System.out.println("\nDFA Example\n");
 
-    Optimize opt = ctx.mkOptimize();
-
-    int n = 2;
-    int aSz = 2;
-    int rSz = 2;
-    Character alphabet[] = {'a', 'b'};
-    int[] acceptingFinalStates = {0};
-    // -1 if no transitions from that state, 0 for 'a' transition, 1 for 'b' transition
-    int[][] acceptingTransitions = {
-      {-1, 0},
-      {0, -1}
-    };
-
-    int[] rejectingFinalStates = {1};
-    // -1 if no transitions from that state, 0 for 'a' transition, 1 for 'b' transition
-    int[][] rejectingTransitions = {
-      {-1, 1},
-      {-1, 1}
-    };
-
-
-    BoolExpr[] finalStates = new BoolExpr[n];
-    // Final states in DFA
-    for (int i = 0; i < n; i++) {
-    	finalStates[i] = ctx.mkBoolConst("f"+i+"");
-    }
-
-    BoolExpr[][] aNFA = new BoolExpr[n][aSz];
+    String sat = "UNSATISFIABLE";
+    int n = 1;
     
-    //match up with accepting NFA
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < aSz; j++) {
-      	 aNFA[i][j] = ctx.mkBoolConst("x"+i+""+j+"p");
-      }
+    // go in a loop and find the min number of states which are satisfiable
+    while (sat.equals("UNSATISFIABLE")) {
+    	Optimize opt = ctx.mkOptimize();
+    	int aSz = 2;
+	    int rSz = 2;
+	    Character alphabet[] = {'a', 'b'};
+	    int[] acceptingFinalStates = {0};
+	    // -1 if no transitions from that state, 0 for 'a' transition, 1 for 'b' transition
+	    int[][] acceptingTransitions = {
+	      {-1, 0},
+	      {0, -1}
+	    };
+	
+	    int[] rejectingFinalStates = {1};
+	    // -1 if no transitions from that state, 0 for 'a' transition, 1 for 'b' transition
+	    int[][] rejectingTransitions = {
+	      {-1, 1},
+	      {-1, 1}
+	    };
+	
+	    // Final states in DFA
+	    BoolExpr[] finalStates = new BoolExpr[n];
+	    for (int i = 0; i < n; i++) {
+	    	finalStates[i] = ctx.mkBoolConst("f"+i+"");
+	    }
+	
+	    //match up with accepting NFA
+	    BoolExpr[][] aNFA = new BoolExpr[n][aSz];
+	    for (int i = 0; i < n; i++) {
+	      for (int j = 0; j < aSz; j++) {
+	      	 aNFA[i][j] = ctx.mkBoolConst("x"+i+""+j+"p");
+	      }
+	    }
+	
+	    //match up with rejecting NFA
+	    BoolExpr[][] rNFA = new BoolExpr[n][rSz];
+	    for (int i = 0; i < n; i++) {
+	      for (int j = 0; j < rSz; j++) {
+	      	 rNFA[i][j] = ctx.mkBoolConst("y"+i+""+j+"pp");
+	      }
+	    }
+	
+	    // transitions in DFA
+	    BoolExpr[][][] trans = new BoolExpr[n][alphabet.length][n];
+	    
+	    for (int i = 0; i < n; i++) {
+	    	 for (int j = 0; j < alphabet.length; j++) { 
+	         	for (int k = 0; k < n; k++) {
+	            	trans[i][j][k] = ctx.mkBoolConst("d"+i+""+alphabet[j]+""+k+"");
+	            }
+	         }
+	    }
+	
+	    // assert DFA not NFA
+	    for (int j = 0; j < alphabet.length; j++) {
+	    	for (int i = 0; i < n; i++) {
+	    		BoolExpr bexp = ctx.mkBool(false);
+	    		for (int k = 0; k < n; k++) {
+	    			bexp = ctx.mkOr(ctx.mkNot(trans[i][j][k]), bexp);
+	    		} 	
+	    		opt.Assert(bexp);
+	    	}
+	    }
+	
+	    // assert DFA's transition function is total (i.e. complete DFA)
+	    for (int j = 0; j < alphabet.length; j++) {
+	    	for (int i = 0; i < n; i++) {
+	    		BoolExpr bexp = ctx.mkBool(false);
+	    		for (int k = 0; k < n; k++) {
+	    			bexp = ctx.mkOr(trans[i][j][k], bexp);
+	    		} 	
+	    		opt.Assert(bexp);
+	    	}
+	    }
+	
+	    // assert start states
+	    opt.Assert(aNFA[0][0]);
+	    opt.Assert(rNFA[0][0]);
+	
+	    // assert transitions from accepting NFA
+	    for (int i = 0; i < n; i++) {
+	    	for (int j = 0; j < aSz; j++) {
+	        	for (int k = 0; k < n; k++) {
+	            	for (int l = 0; l < aSz; l++) {
+	                	if (acceptingTransitions[j][l] >= 0) {
+	                		opt.Assert(ctx.mkImplies(ctx.mkAnd(aNFA[i][j], trans[i][ acceptingTransitions[j][l] ][k]), aNFA[k][l]));
+	                  	}
+	              	}
+	          	}
+	      	}
+	    }
+	
+	    // assert transitions from rejecting NFA
+	    for (int i = 0; i < n; i++) {
+	    	for (int j = 0; j < rSz; j++) {
+	        	for (int k = 0; k < n; k++) {
+	            	for (int l = 0; l < rSz; l++) {
+	                	if (rejectingTransitions[j][l] >= 0) {
+	                    	opt.Assert(ctx.mkImplies(ctx.mkAnd(rNFA[i][j], trans[i][ rejectingTransitions[j][l] ][k]), rNFA[k][l]));
+	                  	}
+	              	}
+	          	}
+	      	}
+	    }
+	
+	    // add final-state constraints
+	    for (int i = 0; i < n; i++) {
+	        for (int j = 0; j < acceptingFinalStates.length; j++) {
+	              opt.Assert(ctx.mkImplies(aNFA[i][ acceptingFinalStates[j]], finalStates[i]));       	 
+	        }
+	      }
+	    
+	    for (int i = 0; i < n; i++) {
+	    	for (int j = 0; j < rejectingFinalStates.length; j++) {
+	        	opt.Assert(ctx.mkImplies(rNFA[i][rejectingFinalStates[j]], ctx.mkNot(finalStates[i])));
+	        }
+	    }
+	
+	    // satisfiable or unsatisfiable
+	    sat = opt.Check().toString();
+		if (sat.equals("SATISFIABLE")) {
+		    System.out.println(sat + " with " + n + " states"); 
+		    System.out.println(opt.getModel());
+		    System.out.println("\n--------------------------------------------------\n");
+		}
+	    n++;
     }
-
-    BoolExpr[][] rNFA = new BoolExpr[n][rSz];
-    
-    //match up with rejecting NFA
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < rSz; j++) {
-      	 rNFA[i][j] = ctx.mkBoolConst("y"+i+""+j+"pp");
-      }
-    }
-
-    // transitions in DFA
-    BoolExpr[][][] trans = new BoolExpr[n][alphabet.length][n];
-    
-    for (int i = 0; i < n; i++) {
-    	 for (int j = 0; j < alphabet.length; j++) { 
-         	for (int k = 0; k < n; k++) {
-            	trans[i][j][k] = ctx.mkBoolConst("d"+i+""+alphabet[j]+""+k+"");
-            }
-         }
-    }
-
-    // assert DFA not NFA
-    for (int j = 0; j < alphabet.length; j++) {
-    	for (int i = 0; i < n; i++) {
-    		BoolExpr bexp = ctx.mkBool(false);
-    		for (int k = 0; k < n; k++) {
-    			bexp = ctx.mkOr(ctx.mkNot(trans[i][j][k]), bexp);
-    		} 	
-    		opt.Assert(bexp);
-    	}
-    }
-
-    // assert DFA's transition function is total (i.e. complete DFA)
-    for (int j = 0; j < alphabet.length; j++) {
-    	for (int i = 0; i < n; i++) {
-    		BoolExpr bexp = ctx.mkBool(false);
-    		for (int k = 0; k < n; k++) {
-    			bexp = ctx.mkOr(trans[i][j][k], bexp);
-    		} 	
-    		opt.Assert(bexp);
-    	}
-    }
-
-    // assert start states
-    opt.Assert(aNFA[0][0]);
-    opt.Assert(rNFA[0][0]);
-
-    // assert transitions from accepting NFA
-    for (int i = 0; i < n; i++) {
-    	for (int j = 0; j < aSz; j++) {
-        	for (int k = 0; k < n; k++) {
-            	for (int l = 0; l < aSz; l++) {
-                	if (acceptingTransitions[j][l] >= 0) {
-                		opt.Assert(ctx.mkImplies(ctx.mkAnd(aNFA[i][j], trans[i][ acceptingTransitions[j][l] ][k]), aNFA[k][l]));
-                  	}
-              	}
-          	}
-      	}
-    }
-
-    // assert transitions from rejecting NFA
-    for (int i = 0; i < n; i++) {
-    	for (int j = 0; j < rSz; j++) {
-        	for (int k = 0; k < n; k++) {
-            	for (int l = 0; l < rSz; l++) {
-                	if (rejectingTransitions[j][l] >= 0) {
-                    	opt.Assert(ctx.mkImplies(ctx.mkAnd(rNFA[i][j], trans[i][ rejectingTransitions[j][l] ][k]), rNFA[k][l]));
-                  	}
-              	}
-          	}
-      	}
-    }
-
-    // add final-state constraints
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < acceptingFinalStates.length; j++) {
-              opt.Assert(ctx.mkImplies(aNFA[i][ acceptingFinalStates[j]], finalStates[i]));       	 
-        }
-      }
-    
-    for (int i = 0; i < n; i++) {
-    	for (int j = 0; j < rejectingFinalStates.length; j++) {
-        	opt.Assert(ctx.mkImplies(rNFA[i][rejectingFinalStates[j]], ctx.mkNot(finalStates[i])));
-        }
-    }
-
-    System.out.println(opt.Check()); 
-    System.out.println(opt.getModel());
-    System.out.println("\n--------------------------------------------------\n");
   }
 
    public static void main(String[] args)
